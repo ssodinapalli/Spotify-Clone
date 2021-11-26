075a864bb4beb4fc0dbc4dab2cb4af0ed817ca1b
jplox <rpawar@innominds.com>
2021-11-26 16:36:10 +0530

    minor changes

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,11 +1,11 @@
-import Signuppage from "./MyComponents/Signuppage";
+
 import LandingPage from "./MyComponents/LandingPage";
 
 function App(){
   return(
     <>
-    {/* <LandingPage></LandingPage> */}
-    <Signuppage></Signuppage>
+    <LandingPage></LandingPage>
+
    
     </>
   );
